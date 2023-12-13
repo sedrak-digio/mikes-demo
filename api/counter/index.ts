ca8e77735289dc192d4690a56fe38f90a11404e1
Mark Sedrak <mark.sedrak@digio.com.au>
2023-12-13 16:03:08 -0300

    feat: connect api to cosmos db

--- api/counter/index.ts
+++ api/counter/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import getDBContainer from "../utils/getDbClient";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -6,10 +7,35 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const response: { [key: string]: any } = { responseMessage }
+
+    try {
+        const dbClient = await getDBContainer('Workshop', 'Counters');
+        const countersRef = await dbClient.item('hungry-counter');
+
+
+        const { resource: counters } = await countersRef.read();
+        response.counters = counters;
+        
+        const counterValue = counters ? counters.counter + 1 : 1;
+
+        if(counters) {
+            await countersRef.replace({ id: 'hungry-counter', counter: counterValue })
+        } else {
+            await dbClient.items.create({id: 'hungry-counter', counter: counterValue})
+        }
+        
+        
+        response.counterValue = counterValue;
+
+    } catch (e) {
+        context.log('HTTP trigger function failed to connect to DB request.', e);
+        response.errors = 'error goes here';
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: response
     };
 
 };
